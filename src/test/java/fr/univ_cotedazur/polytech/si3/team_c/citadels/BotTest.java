c525892e9e2542845201b97383d55e0acad9fb37
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2023-12-01 22:22:48 +0100

    test(#3): Add some initial tests for bots

---
+++ src/test/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/BotTest.java
@@ -0,0 +1,64 @@
+package fr.univ_cotedazur.polytech.si3.team_c.citadels;
+
+import fr.univ_cotedazur.polytech.si3.team_c.citadels.characters.Architect;
+import fr.univ_cotedazur.polytech.si3.team_c.citadels.characters.Magician;
+import fr.univ_cotedazur.polytech.si3.team_c.citadels.districts.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BotTest {
+
+    Player player1;
+
+    @BeforeEach
+    void setUp() {
+        player1 = new Bot("Bot 1", 2, List.of(new Battlefield(), new Castle(), new Church(), new DragonGate()));
+    }
+
+    @Test
+    void coins() {
+        assertEquals(2, player1.getCoins());
+        assertFalse(player1.pay(3));
+        assertTrue(player1.pay(2));
+        assertFalse(player1.pay(-1));
+        assertEquals(0, player1.getCoins());
+        assertTrue(player1.pickCoins()); // The player has no money and should therefore get some gold
+    }
+
+    @Test
+    void pickCharacter() {
+        assertTrue(player1.getCharacter().isEmpty());
+        assertEquals(new Architect(), player1.pickCharacter(List.of(new Architect())));
+        assertEquals(new Architect(), player1.getCharacter().get());
+        assertEquals(new Magician(), player1.pickCharacter(List.of(new Magician())));
+        assertEquals(new Magician(), player1.getCharacter().get());
+    }
+
+    @Test
+    void pickDistrictsFromDeck() {
+        // The player already has a castle in hand, since duplicates are not allowed, he should take the other one
+        assertEquals(List.of(new Cathedral()), player1.pickDistrictsFromDeck(List.of(new Castle(), new Cathedral()), 1));
+
+        // The player already has a castle in hand, he should take the other two
+        assertEquals(List.of(new Fortress(), new Cathedral()), player1.pickDistrictsFromDeck(List.of(new Fortress(), new Cathedral(), new Church()), 2));
+
+        // The player has no choice but to take a church, even if he already has one
+        assertEquals(List.of(new Church()), player1.pickDistrictsFromDeck(List.of(new Church(), new Church()), 1));
+
+        assertEquals(1, player1.pickDistrictsFromDeck(List.of(new Fortress(), new Cathedral()), 1).size());
+        assertEquals(2, player1.pickDistrictsFromDeck(List.of(new Fortress(), new Cathedral()), 2).size());
+        assertEquals(1, player1.pickDistrictsFromDeck(List.of(new Fortress(), new Cathedral(), new Graveyard()), 1).size());
+    }
+
+    @Test
+    void pickDistrictsToBuild() {
+        assertEquals(List.of(new Battlefield(), new Castle(), new Church(), new DragonGate()), player1.getHandDistricts());
+        assertTrue(player1.getBuiltDistricts().isEmpty());
+        assertEquals(List.of(new Church()), player1.pickDistrictsToBuild(1)); // Only the church is affordable for the player
+        assertEquals(List.of(new Church()), player1.getBuiltDistricts());
+    }
+}
