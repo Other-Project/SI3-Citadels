4a8730c3a9fe7040a86ad9ef4a78a3d0e83c2d65
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2023-12-01 13:33:42 +0100

    Update maven configuration (#1)

---
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Game.java
@@ -0,0 +1,7 @@
+package fr.univ_cotedazur.polytech.si3.team_c.citadels;
+
+public class Game {
+    public static void main(String... args) {
+
+    }
+}
---
+++ src/test/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/GameTest.java
@@ -0,0 +1,10 @@
+package fr.univ_cotedazur.polytech.si3.team_c.citadels;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class GameTest {
+    @BeforeEach
+    void setup() {
+
+    }
+}
