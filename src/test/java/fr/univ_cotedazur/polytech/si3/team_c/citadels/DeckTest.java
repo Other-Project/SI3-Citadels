e79a0912be29dd6453d8efaca477fbca63061d06
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2023-12-05 16:25:21 +0100

    feat(#16): Updated the class DeckTest by adding some tests

--- src/test/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/DeckTest.java
+++ src/test/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/DeckTest.java
@@ -2,13 +2,15 @@ package fr.univ_cotedazur.polytech.si3.team_c.citadels;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DeckTest {
     Deck deck = new Deck();
 
     @Test
-    void getDeck() {
-        assertEquals(65, deck.getDeck().size());
+    void draw() {
+        assertEquals(List.of(deck.getFirst()), deck.draw(1));
     }
 }
