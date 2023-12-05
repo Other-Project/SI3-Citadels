39494cbcb379c22d69caf20b98c60271ac8fd703
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2023-12-05 13:19:20 +0100

    refactor(#8): Simplify logger's output format
    Closes #8

--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Game.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Game.java
@@ -100,6 +100,7 @@ public class Game {
     }
 
     public static void main(String... args) {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "-%4$s- %5$s%6$s%n");
         new Game().start();
     }
 }
