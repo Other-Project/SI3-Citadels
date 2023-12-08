53dd90179cb81051bf72f25c7bc2707f89c38a74
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2023-12-08 16:17:37 +0100

    comment(#19): Updated the Actions enum by adding comments and the SPECIAL_INCOME action

--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Action.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Action.java
@@ -1,5 +1,24 @@
 package fr.univ_cotedazur.polytech.si3.team_c.citadels;
 
 public enum Action {
-    INCOME, DRAW, BUILD, NONE
+    /**
+     * The Default Income : the player can choose this action to get 2 coins
+     */
+    INCOME,
+    /**
+     * It's a bonus incoe according to the player's character
+     */
+    SPECIAL_INCOME,
+    /**
+     * The default drawing : the player chooses a card between two from the deck
+     */
+    DRAW,
+    /**
+     * The build action : the player builds a district from his hand according to its cost
+     */
+    BUILD,
+    /**
+     * The action of stopping the layer's turn
+     */
+    NONE
 }
