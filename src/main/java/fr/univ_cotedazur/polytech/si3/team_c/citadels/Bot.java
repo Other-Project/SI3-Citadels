4e93cfb3f7b27a9a64e8d891318c43ac0e37ba1b
Evan Galli <evan.galli@etu.univ-cotedazur.fr>
2023-12-15 15:38:36 +0100

    fix(#9): Fix import

--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Bot.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Bot.java
@@ -1,10 +1,8 @@
 package fr.univ_cotedazur.polytech.si3.team_c.citadels;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.function.DoubleSupplier;
+import java.util.function.Function;
 
 /**
  * Rebot player
