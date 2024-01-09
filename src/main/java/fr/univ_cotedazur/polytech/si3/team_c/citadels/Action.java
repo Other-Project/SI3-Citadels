451f3003b2af6d4d6af10661638720033a068d64
LubratJilian <jilian.lubrat@etu.unice.fr>
2024-01-09 23:30:43 +0100

    Feature (#36): add a crown linked start of turn Action for the `King`

--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Action.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Action.java
@@ -54,6 +54,10 @@ public enum Action {
      */
     STARTUP_INCOME,
     /**
+     * The player gets the crown at the beginning of his turn
+     */
+    GET_CROWN,
+    /**
      * The player wants to end his turn
      */
     NONE
--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Game.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/Game.java
@@ -128,7 +128,6 @@ public class Game {
         LOGGER.info(player::toString);
         player.createActionSet();
         charactersToInteractWith.remove(player.getCharacter().orElseThrow());
-        if (player.getCharacter().orElseThrow() instanceof King) setCrown(playerList.indexOf(player));
         if (player.getCharacter().orElseThrow().equals(characterToRob)) {
             LOGGER.log(Level.INFO, "{0} was robbed because he was the {1}", new Object[]{player.getName(), characterToRob});
             LOGGER.log(Level.INFO, "{0} gains {1} coins from {2} and has now {3} coins",
@@ -155,6 +154,10 @@ public class Game {
                 case STARTUP_INCOME -> {
                     LOGGER.log(Level.INFO, "{0} earned a coin because he was the {1}", new Object[]{player.getName(), player.getCharacter().orElseThrow()});
                     player.gainCoins(1);
+                }
+                case GET_CROWN -> {
+                    LOGGER.log(Level.INFO, "{0} get the crown because he was the {1}", new Object[]{player.getName(), player.getCharacter().orElseThrow()});
+                    setCrown(playerList.indexOf(player));
                 }
                 default ->
                         throw new UnsupportedOperationException("The start-of-turn action " + startOfTurnAction + " has not yet been implemented");
