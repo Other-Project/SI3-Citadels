4a2b2bbd5a077935dec57287c2729b637d995f70
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-01-26 15:59:56 +0100

    feat(#96) : the discreet bot now attempts a final rush when it is near to the end

--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/players/DiscreetBot.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/players/DiscreetBot.java
@@ -1,7 +1,10 @@
 package fr.univ_cotedazur.polytech.si3.team_c.citadels.players;
 
 import fr.univ_cotedazur.polytech.si3.team_c.citadels.District;
+import fr.univ_cotedazur.polytech.si3.team_c.citadels.Game;
+import fr.univ_cotedazur.polytech.si3.team_c.citadels.IPlayer;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class DiscreetBot extends Bot {
@@ -31,6 +34,12 @@ public class DiscreetBot extends Bot {
         return -10;
     }
 
+    @Override
+    protected double numberOfDistrictToBuildMultiplier() {
+        // We increase the coefficient relating to the number of district we can build according to tryFinalRush
+        return tryFinalRush() ? super.numberOfDistrictToBuildMultiplier() * 9999 : super.numberOfDistrictToBuildMultiplier();
+    }
+
     public DiscreetBot(String name) {
         super(name);
     }
@@ -56,5 +65,17 @@ public class DiscreetBot extends Bot {
      */
     private double bonusProfitability(District district) {
         return (getBuiltDistricts().size() + 1.0) / (getBuiltDistricts().stream().filter(district1 -> district1.getColor() == district.getColor()).count() + 1 << 2);
+    }
+
+    private boolean tryFinalRush() {
+        return Game.DISTRICT_NUMBER_TO_END - getHandSize() > 1
+                && Game.DISTRICT_NUMBER_TO_END - getHandSize() <= 3
+                && getCoins() >= 4;
+    }
+
+    private int differenceOfDistrictsWithFirst() {
+        IPlayer firstPlayer = getPlayers().stream().sorted(
+                Comparator.<IPlayer>comparingInt(player -> player.getBuiltDistricts().size()).reversed()).toList().get(0);
+        return firstPlayer.getHandSize() - getHandSize();
     }
 }
