105a1760364faf2eab47fe53b82e211281275d0a
Théo Lassauniere <theo.lassauniere@etu.univ-cotedazur.fr>
2024-02-06 11:10:44 +0100

    feat(#111) : The richardBot now follows the strategy that aims to block the architect if needed

--- src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/players/RichardBot.java
+++ src/main/java/fr/univ_cotedazur/polytech/si3/team_c/citadels/players/RichardBot.java
@@ -20,18 +20,18 @@ public class RichardBot extends Bot {
     @Override
     public Character chooseCharacterToKill(List<Character> characterList) {
         IPlayer enderPlayer = playerCanEndGame().get(0);
-        // If the bot estimates that the ender player has the best chances to get the stealing or destroying power, we kill that character
-        if (enderPlayer != null) {
-            Character enderPlayerCharacter = characterEstimation(enderPlayer, Game.defaultCharacterList());
-            if (enderPlayerCharacter.getAction().contains(Action.STEAL) || enderPlayerCharacter.getAction().contains(Action.DESTROY))
-                return enderPlayerCharacter;
-        }
-
         // If the bot is in first place, he must kill the characters that could destroy one of his districts
         SimpleEntry<IPlayer, Integer> firstPlayer = playerWithMaxAttribute(iPlayer -> iPlayer.getBuiltDistricts().size());
         if (firstPlayer.getKey().equals(this)) {
             var annoyingCharacter = characterList.stream().filter(character -> character.getAction().contains(Action.DESTROY)).findFirst();
             if (annoyingCharacter.isPresent()) return annoyingCharacter.get();
+        }
+
+        // If the bot estimates that the ender player has the best chances to get the stealing or destroying power, we kill that character
+        if (enderPlayer != null) {
+            Character enderPlayerCharacter = characterEstimation(enderPlayer, CharacterManager.defaultCharacterList());
+            if (enderPlayerCharacter.getAction().contains(Action.STEAL) || enderPlayerCharacter.getAction().contains(Action.DESTROY))
+                return enderPlayerCharacter;
         }
 
         // With this condition, a player cannot get too rich by stealing someone
@@ -45,6 +45,18 @@ public class RichardBot extends Bot {
         return super.chooseCharacterToKill(characterList);
     }
 
+    @Override
+    protected double characterProfitability(Character character, CharacterManager characterManager) {
+        if (playerCanAttemptFinalRush(character)) {
+            return 50;
+        }
+        if (character.getAction().contains(Action.KILL) && playerCanAttemptFinalRush(characterManager)) {
+            return 40;
+        }
+
+        return super.characterProfitability(character, characterManager);
+    }
+
     /**
      * This method returns the players that can end the current game
      *
@@ -53,7 +65,7 @@ public class RichardBot extends Bot {
     public List<IPlayer> playerCanEndGame() {
         List<IPlayer> resPlayers = new ArrayList<>();
         for (IPlayer player : getPlayers()) {
-            if (player.getBuiltDistricts().size() + 1 == Game.DISTRICT_NUMBER_TO_END) resPlayers.add(player);
+            if (player.getBuiltDistricts().size() + 1 >= getNumberOfDistrictsToEnd()) resPlayers.add(player);
         }
         return resPlayers;
     }
@@ -65,11 +77,29 @@ public class RichardBot extends Bot {
         int potentialMaxCoins = 0;
         for (int i = 0; i < getPlayers().size() - 1; i++) {
             for (int j = 1; j < getPlayers().size(); j++) {
-                if (getPlayers().get(i).equals(this) || getPlayers().get(j).equals(this)) continue;
                 int currentRes = getPlayers().get(i).getCoins() + getPlayers().get(j).getCoins();
                 if (currentRes >= potentialMaxCoins) potentialMaxCoins = currentRes;
             }
         }
         return potentialMaxCoins;
+    }
+
+    private boolean playerCanAttemptFinalRush(CharacterManager characterManager) {
+        for (Character character : characterManager.charactersList().stream()
+                .filter(character -> !characterManager.getVisible().contains(character)).toList()) {
+            if (playerCanAttemptFinalRush(character)) return true;
+        }
+        return false;
+    }
+
+    private boolean playerCanAttemptFinalRush(Character character) {
+        for (IPlayer player : getPlayers()) {
+            if (player.getCoins() >= 4
+                    && getNumberOfDistrictsToEnd() - player.getBuiltDistricts().size() != 1
+                    && player.getHandSize() >= character.numberOfDistrictToBuild() - (character.startTurnAction().equals(Action.BEGIN_DRAW) ? 2 : 0)
+                    && player.getBuiltDistricts().size() >= getNumberOfDistrictsToEnd() - character.numberOfDistrictToBuild())
+                return true;
+        }
+        return false;
     }
 }
